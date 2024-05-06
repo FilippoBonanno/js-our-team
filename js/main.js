aac1bd26ec8f3c5dc3d830c02cf45e5441e8a92c
FilippoBonanno <fbonanno15@gmail.com>
2024-05-06 19:08:38 +0200

    M-2

--- js/main.js
+++ js/main.js
@@ -2,21 +2,32 @@
 
 
 let studenti = [
-	{ nome: "Wayne Barnett", ruolo: "Founder & CEO", immagine: "wayne-barnett-founder-ceo.jpg" },
-	{ nome: "Angela Caroll", ruolo: "Chief Editor", immagine: "angela-caroll-chief-editor.jpg" },
-	{ nome: "Walter Gordon", ruolo: "Office Manager", immagine: "walter-gordon-office-manager.jpg" },
-	{ nome: "Angela Lopez", ruolo: "Social Media Manager", immagine: "angela-lopez-social-media-manager.jpg" },
-	{ nome: "Scott Estrada", ruolo: "Developer", immagine: "scott-estrada-developer.jpg" },
-	{ nome: "Barbara Ramos", ruolo: "Graphic Designer", immagine: "barbara-ramos-graphic-designer.jpg" }
+	{ nome: "Wayne Barnett", ruolo: "Founder & CEO", immagine: "./img/wayne-barnett-founder-ceo.jpg" },
+	{ nome: "Angela Caroll", ruolo: "Chief Editor", immagine: "./img/angela-caroll-chief-editor.jpg" },
+	{ nome: "Walter Gordon", ruolo: "Office Manager", immagine: "./img/walter-gordon-office-manager.jpg" },
+	{ nome: "Angela Lopez", ruolo: "Social Media Manager", immagine: "./img/angela-lopez-social-media-manager.jpg" },
+	{ nome: "Scott Estrada", ruolo: "Developer", immagine: "./img/scott-estrada-developer.jpg" },
+	{ nome: "Barbara Ramos", ruolo: "Graphic Designer", immagine: "./img/barbara-ramos-graphic-designer.jpg" }
 ];
 
 
 
- for (let i = 0; i < studenti.length; i++) {
-     const player = studenti[i];
-      console.log(`Nome giocatore: ${player.nome}, Ruolo: ${player.ruolo}`);
-      
- }
+
+const ulList = document.getElementById("lista");
+
+for (let i = 0; i < studenti.length; i++) {
+    const player = studenti[i];
+    console.log(`Nome giocatore: ${player.nome}, Ruolo: ${player.ruolo}`);
+
+    // Creiamo una stringa HTML per rappresentare l'elemento <li> con le informazioni del giocatore
+    const li = `<li>Nome giocatore: ${player.nome}, Ruolo: ${player.ruolo}, <img src="${player.immagine}"></li>`;
+
+    // Aggiungiamo la stringa HTML alla proprietà innerHTML dell'elemento <ul>
+    ulList.innerHTML += li;
+}
+
+
+ 
 
 
 
